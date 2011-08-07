951dd06d1f8562418185e25d67d07c067cbcd20b
Michael Huynh <mike.huynh@gmail.com>
2011-08-07 14:04:20 -0400

    Added blank dashboard page.

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('base.views',
     # Examples:
     url(r'^$', 'home', name='home'),
+    url(r'^dashboard/$', 'dashboard', name='dashboard'),
 )
--- base/views.py
+++ base/views.py
@@ -1,7 +1,16 @@
+from django.contrib.auth.decorators import login_required
+
 from annoying.decorators import render_to, ajax_request
 #from annoying.functions import get_object_or_None
 
 @render_to('base/home.html')
 def home(request):
+
+    return {}
+
+
+@login_required
+@render_to('base/dashboard.html')
+def dashboard(request):
 
     return {}
