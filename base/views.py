aff1c9a3aee09e3b53e8c2715a7c701ba29ff0db
Michael Huynh <mike.huynh@gmail.com>
2011-08-07 18:39:00 -0400

    Added parameters to end of webchat url to automatically sign into server and join room.

--- base/views.py
+++ base/views.py
@@ -5,6 +5,8 @@ from django.conf import settings
 from annoying.decorators import render_to, ajax_request
 #from annoying.functions import get_object_or_None
 
+import urllib
+
 @render_to('base/home.html')
 def home(request):
 
@@ -23,6 +25,13 @@ def dashboard(request):
 def chat(request):
     webchat_url = getattr(settings, 'WEBCHAT_URL', 
             'http://%s:%s/' % (request.META['SERVER_NAME'], 9090))
+
+    #Now add user-specific connection parameters on the url
+    webchat_url += '?%s' % urllib.urlencode({
+        'nick': request.user.username,
+        'channels': '#mit',
+        'key': 'secret',
+    })
 
     return redirect(webchat_url)
 
