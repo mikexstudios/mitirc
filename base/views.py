ccc6f48795d4f934982b83afdfb4623eff7339c8
Michael Huynh <mike.huynh@gmail.com>
2011-08-07 21:34:28 -0400

    Now passing IRC password to qwebirc client.

--- base/views.py
+++ base/views.py
@@ -5,6 +5,8 @@ from django.conf import settings
 from annoying.decorators import render_to, ajax_request
 #from annoying.functions import get_object_or_None
 
+#from base import models
+
 import urllib
 
 @render_to('base/home.html')
@@ -26,11 +28,14 @@ def chat(request):
     webchat_url = getattr(settings, 'WEBCHAT_URL', 
             'http://%s:%s/' % (request.META['SERVER_NAME'], 9090))
 
+    #Get user's IRC authentication information
+    ia = request.user.ircauth
+
     #Now add user-specific connection parameters on the url
     webchat_url += '?%s' % urllib.urlencode({
         'nick': request.user.username,
         'channels': '#mit',
-        'key': 'secret',
+        'key': ia.password,
     })
 
     return redirect(webchat_url)
