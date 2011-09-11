8a6d8feb2d9367127a3ee760299367bcca81338b
Michael Huynh <mike.huynh@gmail.com>
2011-09-11 17:40:48 -0400

    Using South for migrations now since models have gotten slightly more complicated.

--- settings.py
+++ settings.py
@@ -131,6 +131,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 
+    'south', #database migrations
     #'djcelery', #messaging queue
     #'djkombu', #for using orm as ghetto queue
 
