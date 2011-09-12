0742448f24bfbff8b21fcf508a800b891da4cadf
Michael Huynh <mike.huynh@gmail.com>
2011-09-11 21:19:45 -0400

    Added note about superuser creation error before migrating databases.

--- base/handlers.py
+++ base/handlers.py
@@ -7,6 +7,11 @@ def create_ircauth(sender, **kw):
     '''
     Creates an IRCAuth entry for the user if this is the first time that the user
     is created.
+
+    WARNING: Because we use South to manage base-related tables, when superuser is
+    created through syncdb, this handler gets called and errors because IRCAuth
+    does not exist. Thus, the workaround is to create the superuser *after* running
+    both syncdb and migrations.
     '''
     user = kw["instance"]
     if kw["created"]:
